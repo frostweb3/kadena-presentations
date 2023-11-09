9ebb90043297df096da4ca87ce0fedccae350bd6
John Frost <waimea@duck.com>
2023-11-09 12:40:41 -0800

    Move shared files to root

--- gen-watch.sh
+++ gen-watch.sh
@@ -2,4 +2,4 @@
 
 # sleep 0.1s fixes some bugs with partially rendered content
 # when the slidy template was too large (we had inlined images)
-npx nodemon -e md,slidy --exec "sleep 0.1 && ./gen.sh"
+npx nodemon -e md,slidy --exec "sleep 0.1 && ../gen.sh"
